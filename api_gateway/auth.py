6cd05f7156a99edfbdcfc017e70736d21379591f
loures <loures.raso@gmail.com>
2020-11-22 19:47:39 +0100

    Fix expired token issue

--- api_gateway/auth.py
+++ api_gateway/auth.py
@@ -1,16 +1,19 @@
 import functools
 from api_gateway.classes.user import User
 from flask_jwt_extended import jwt_required, JWTManager, current_user, jwt_optional
+from flask import redirect, url_for, make_response
 
 jwt_manager = JWTManager()
 currently_logged_in = {}
 
 current_user = current_user
 
+
 @jwt_manager.unauthorized_loader
 def unauthorized(error_description):
     # TODO: return stuff
     return "Unauthorized", 401
+
 
 @jwt_manager.user_loader_callback_loader
 def loader(user_id):
@@ -23,9 +26,22 @@ def loader(user_id):
             return {}
     return currently_logged_in.get(str(user_id))
 
+
+@jwt_manager.expired_token_loader
+def expired_token_callback():
+    # Reset the cookies
+    response = make_response()
+    response.set_cookie('gooutsafe_jwt_token', "", expires=0)
+    response.set_cookie("csrf_access_token", "", expires=0)
+    response.status_code = 301
+    response.location = url_for("auth.login")
+    return response
+
+
 # flask_jwt_extended fai cacare
 @jwt_optional
 def user_loader_ctx_processor():
+    # Pass the JWT current_user as current_user like flask-login does
     return dict(current_user=current_user)
 
 
@@ -51,5 +67,6 @@ def operator_required(func):
         return func(*args, **kw)
 
     return _operator_required
+
 
 login_required = jwt_required
